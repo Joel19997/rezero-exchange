22991b03622221643e343df6c1d54ef5d8ea0c48
Joel19997 <joelseah97@gmail.com>
2020-11-08 17:59:50 +0800

    search.html

--- html/database/getListingByOwner.php
+++ html/database/getListingByOwner.php
@@ -5,10 +5,13 @@
     $searching = $_GET["owner_email"];
 
     $result = $dao->getListingByOwner($searching);
+    $searched_books = [];
     for($i = 0; $i < count($result); $i++)
     {
-        echo $result[$i]->getTitle();
-        echo "</br>";
-
+        //$result[$i]->getTitle();
+        $temp_Lid = $result[$i]->getLid();
+        array_push($searched_books,$temp_Lid);
     }
+    var_dump($searched_books);
+
 ?>
